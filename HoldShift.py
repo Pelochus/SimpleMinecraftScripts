ff068c4f3036f1c405bc7035c5f4a7519b094b57
Pelochus <49835792+Pelochus@users.noreply.github.com>
2021-02-16 17:33:51 +0100

    Added main scripts

---
+++ HoldShift.py
@@ -0,0 +1,84 @@
+# Holds shift in Minecraft to avoid falling or similar 
+
+import time
+import os
+import ctypes
+
+# ---------- Classes and Functions ----------
+
+# This was taken from a webpage a long time ago, I can't explain how it works
+# It makes Minecraft understand key presses from the script
+# PyAutoGUI wasnt capable of this 
+
+PUL = ctypes.POINTER(ctypes.c_ulong)
+
+class KeyBdInput(ctypes.Structure):
+   _fields_ = [("wVk", ctypes.c_ushort),
+               ("wScan", ctypes.c_ushort),
+               ("dwFlags", ctypes.c_ulong),
+               ("time", ctypes.c_ulong),
+               ("dwExtraInfo", PUL)]
+
+
+class HardwareInput(ctypes.Structure):
+   _fields_ = [("uMsg", ctypes.c_ulong),
+               ("wParamL", ctypes.c_short),
+               ("wParamH", ctypes.c_ushort)]
+
+
+class MouseInput(ctypes.Structure):
+   _fields_ = [("dx", ctypes.c_long),
+               ("dy", ctypes.c_long),
+               ("mouseData", ctypes.c_ulong),
+               ("dwFlags", ctypes.c_ulong),
+               ("time", ctypes.c_ulong),
+               ("dwExtraInfo", PUL)]
+
+
+class Input_I(ctypes.Union):
+   _fields_ = [("ki", KeyBdInput),
+               ("mi", MouseInput),
+               ("hi", HardwareInput)]
+
+
+class Input(ctypes.Structure):
+   _fields_ = [("type", ctypes.c_ulong),
+("ii", Input_I)]
+
+def press_key(key):
+   extra = ctypes.c_ulong(0)
+   ii_ = Input_I()
+
+   flags = 0x0008
+
+   ii_.ki = KeyBdInput(0, key, flags, 0, ctypes.pointer(extra))
+   x = Input(ctypes.c_ulong(1), ii_)
+   ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+def release_key(key):
+   extra = ctypes.c_ulong(0)
+   ii_ = Input_I()
+
+   flags = 0x0008 | 0x0002
+
+   ii_.ki = KeyBdInput(0, key, flags, 0, ctypes.pointer(extra))
+   x = Input(ctypes.c_ulong(1), ii_)
+   ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+# ----------- Main -----------
+	
+shift = 0x2A # See: https://gist.github.com/tracend/912308
+duration = 25
+elapsed = 0
+
+time.sleep(4) # Delay before starting 
+print("Working...")
+
+while elapsed < duration:
+    press_key(shift)
+    time.sleep(1)
+    elapsed = elapsed + 1 
+    
+release_key(shift)
+print("Finished")
+time.sleep(5)
